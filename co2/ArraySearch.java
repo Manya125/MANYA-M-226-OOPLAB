a007004258b3311b9255df20a5ef3f82dbe95392
Manya M <93362285+Manya125@users.noreply.github.com>
2022-04-07 15:59:16 +0530

    Add files via upload

---
+++ co2/ArraySearch.java
@@ -0,0 +1,22 @@
+package mca226;
+import java.util.*;
+public class ArraySearch {
+	
+	public static void main ( String [] args) {
+		Scanner s=new Scanner(System.in);
+		int array[]=new int[10];
+		for(int i=0;i<array.length;i++) {
+			
+			System.out.println("enter array element");
+			int s=s.nextInt();
+		}
+		
+		for(int i=0;i<array.length;i++) {
+			System.out.println("element at index" + i + ":" +array[i]);
+		}
+		
+		
+		
+
+	}
+}
