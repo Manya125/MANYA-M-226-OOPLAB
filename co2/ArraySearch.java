1b8d6b3baddb51795594b9612cf313b73ea8aa30
Manya M <93362285+Manya125@users.noreply.github.com>
2022-04-11 10:45:51 +0530

    program2 co2

--- co2/ArraySearch.java
+++ co2/ArraySearch.java
@@ -1,19 +1,36 @@
 package mca226;
-import java.util.*;
+import java.util.Scanner;
 public class ArraySearch {
+	public void search(int element,int size,int array[])
+	{
+		for(int i=0;i<size;i++)
+		{
+			if( array[i] == element )
+			{
+				i++;
+				System.out.println("elemt found at position:" + i);
+				break;
+			}
+		}
+	}
 	
 	public static void main ( String [] args) {
 		Scanner s=new Scanner(System.in);
+		ArraySearch obj=new ArraySearch();
 		int array[]=new int[10];
-		for(int i=0;i<array.length;i++) {
+		System.out.println("Enter the array size:");
+	    	int size = s.nextInt();
+		for(int i=0;i<size;i++) {
 			
 			System.out.println("enter array element");
-			int s=s.nextInt();
+			array[i]=s.nextInt();
 		}
-		
-		for(int i=0;i<array.length;i++) {
+		for(int i=0;i<size;i++) {
 			System.out.println("element at index" + i + ":" +array[i]);
 		}
+		System.out.println("enter the element to be searched");
+		int element=s.nextInt();
+		obj.search(element,size,array);
 		
 		
 		
